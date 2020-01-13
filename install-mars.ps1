7b633bbadc311d657e9d081836a91fff1564f796
Hinaka Yuuki <thedatahord@yahoo.co.jp>
2020-01-13 12:18:54 +0300

    Add files via upload

---
+++ install-mars.ps1
@@ -0,0 +1,223 @@
+﻿# Ver 1.2
+# Выполнить перед запуском скрипта "Set-ExecutionPolicy Unrestricted"
+# Открепить все ярлыки от начального экрана
+$tilecollection = Get-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\CloudStore\Store\Cache\DefaultAccount\*start.tilegrid`$windows.data.curatedtilecollection.tilecollection\Current
+$unpin = $tilecollection.Data[0..25] + ([byte[]](202,50,0,226,44,1,1,0,0))
+New-ItemProperty -Path $tilecollection.PSPath -Name Data -PropertyType Binary -Value $unpin -Force
+
+# Открепить Microsoft Edge и Microsoft Store от панели задач
+$Signature = @{
+	Namespace = "WinAPI"
+	Name = "GetStr"
+	Language = "CSharp"
+	MemberDefinition = @"
+		[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+		public static extern IntPtr GetModuleHandle(string lpModuleName);
+		[DllImport("user32.dll", CharSet = CharSet.Auto)]
+		internal static extern int LoadString(IntPtr hInstance, uint uID, StringBuilder lpBuffer, int nBufferMax);
+		public static string GetString(uint strId)
+		{
+			IntPtr intPtr = GetModuleHandle("shell32.dll");
+			StringBuilder sb = new StringBuilder(255);
+			LoadString(intPtr, strId, sb, sb.Capacity);
+			return sb.ToString();
+		}
+"@
+}
+IF (-not ("WinAPI.GetStr" -as [type]))
+{
+	Add-Type @Signature -Using System.Text
+}
+$unpin = [WinAPI.GetStr]::GetString(5387)
+$apps = (New-Object -ComObject Shell.Application).NameSpace("shell:::{4234d49b-0245-4df3-b780-3893943456e1}").Items()
+$apps | Where-Object -FilterScript {$_.Path -like "Microsoft.MicrosoftEdge*"} | ForEach-Object -Process {$_.Verbs() | Where-Object -FilterScript {$_.Name -eq $unpin} | ForEach-Object -Process {$_.DoIt()}}
+$apps | Where-Object -FilterScript {$_.Path -like "Microsoft.WindowsStore*"} | ForEach-Object -Process {$_.Verbs() | Where-Object -FilterScript {$_.Name -eq $unpin} | ForEach-Object -Process {$_.DoIt()}}
+
+# Удаление стандартных UPW прилдожении Microsoft
+Get-AppxPackage *bing* | Remove-AppxPackage
+Get-AppxPackage *3dbuilder* | Remove-AppxPackage
+Get-AppxPackage *windowsalarms* | Remove-AppxPackage
+Get-AppxPackage *windowscalculator* | Remove-AppxPackage
+Get-AppxPackage *windowscommunicationsapps* | Remove-AppxPackage
+Get-AppxPackage *windowscamera* | Remove-AppxPackage
+Get-AppxPackage *officehub* | Remove-AppxPackage
+Get-AppxPackage *skypeapp* | Remove-AppxPackage
+Get-AppxPackage *getstarted* | Remove-AppxPackage
+Get-AppxPackage *zunemusic* | Remove-AppxPackage
+Get-AppxPackage *windowsmaps* | Remove-AppxPackage
+Get-AppxPackage *zunevideo* | Remove-AppxPackage
+Get-AppxPackage *bingnews* | Remove-AppxPackage
+Get-AppxPackage *onenote* | Remove-AppxPackage
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+Get-AppxPackage *bingsports* | Remove-AppxPackage
+Get-AppxPackage *soundrecorder* | Remove-AppxPackage
+Get-AppxPackage *bingweather* | Remove-AppxPackage
+Get-AppxPackage *xboxapp* | Remove-AppxPackage
+Get-AppxPackage *4DF9E0F8.Netflix* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.Microsoft3DViewer* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.MicrosoftOfficeHub* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.MicrosoftSolitaireCollection* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.MicrosoftStickyNotes* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.MSPaint* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.Office.Desktop* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.Print3D* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.SkypeApp* | Remove-AppxPackage
+Get-AppxPackage *windowsphone* | Remove-AppxPackage
+Get-AppxPackage *Yandex* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.ZuneVideo* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.ZuneMusic* | Remove-AppxPackage
+Get-AppxPackage *Microsoft.XboxSpeechToTextOverlay* | Remove-AppxPackage
+
+
+# Разархивация МАРС на диск C
+Add-Type -AssemblyName System.IO.Compression.FileSystem
+function Unzip
+{
+    param([string]$zipfile, [string]$outpath)
+
+    [System.IO.Compression.ZipFile]::ExtractToDirectory($zipfile, $outpath)
+}
+Unzip "C:\install\mars.zip" "C:\mars"
+
+# Создание ярлыков MARS
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\Users\admin\Desktop\MARS_TeleShellServer.lnk")
+$Shortcut.TargetPath = "C:\mars\MARS_TeleShellServer.exe"
+$Shortcut.Save()
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\Users\admin\Desktop\MARS_TeleShellClient.lnk")
+$Shortcut.TargetPath = "C:\mars\MARS_TeleShellClient.exe"
+$Shortcut.Save()
+Copy-Item C:\mars\manual\KPASO-R_RA.pdf C:\Users\admin\Desktop\KPASO-R_RA.pdf
+Copy-Item C:\mars\manual\KPASO-R_RO.pdf C:\Users\admin\Desktop\KPASO-R_RO.pdf
+
+# Включение RDP
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
+Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "UserAuthentication" -Value 1
+
+# Отключение спящего режима и дисплея
+powercfg -x standby-timeout-dc 0
+powercfg -x standby-timeout-ac 0
+powercfg -x monitor-timeout-ac 0
+powercfg -x monitor-timeout-dc 0
+
+# Смена картинки на рабочем столе
+Copy-Item C:\install\wall_mars.jpg C:\mars
+add-type @"
+using System;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+namespace Wallpaper
+{
+   public enum Style : int
+   {
+       Tile, Center, Stretch, NoChange
+   }
+
+
+   public class Setter {
+      public const int SetDesktopWallpaper = 20;
+      public const int UpdateIniFile = 0x01;
+      public const int SendWinIniChange = 0x02;
+
+      [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+      private static extern int SystemParametersInfo (int uAction, int uParam, string lpvParam, int fuWinIni);
+      
+      public static void SetWallpaper ( string path, Wallpaper.Style style ) {
+         SystemParametersInfo( SetDesktopWallpaper, 0, path, UpdateIniFile | SendWinIniChange );
+         
+         RegistryKey key = Registry.CurrentUser.OpenSubKey("Control Panel\\Desktop", true);
+         switch( style )
+         {
+            case Style.Stretch :
+               key.SetValue(@"WallpaperStyle", "2") ; 
+               key.SetValue(@"TileWallpaper", "0") ;
+               break;
+            case Style.Center :
+               key.SetValue(@"WallpaperStyle", "1") ; 
+               key.SetValue(@"TileWallpaper", "0") ; 
+               break;
+            case Style.Tile :
+               key.SetValue(@"WallpaperStyle", "1") ; 
+               key.SetValue(@"TileWallpaper", "1") ;
+               break;
+            case Style.NoChange :
+               break;
+         }
+         key.Close();
+      }
+   }
+}
+"@
+Get-ChildItem "C:\mars\wall_mars.jpg" -Recurse  | Get-Random -Count 1  | Foreach {[Wallpaper.Setter]::SetWallpaper( $_.FullName, "NoChange" )}
+
+# Отключить Запись и трансляции игр Windows
+IF (-not (Test-Path -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR))
+{
+	New-Item -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR -Force
+}
+New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Microsoft\Windows\GameDVR -Name AllowgameDVR -PropertyType DWord -Value 0 -Force
+# Отключить игровую панель
+New-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\GameDVR -Name AppCaptureEnabled -PropertyType DWord -Value 0 -Force
+New-ItemProperty -Path HKCU:\System\GameConfigStore -Name GameDVR_Enabled -PropertyType DWord -Value 0 -Force
+# Отключить игровой режим
+New-ItemProperty -Path HKCU:\Software\Microsoft\GameBar -Name AllowAutoGameMode -PropertyType DWord -Value 0 -Force
+# Отключить подсказки игровой панели
+New-ItemProperty -Path HKCU:\Software\Microsoft\GameBar -Name ShowStartupPanel -PropertyType DWord -Value 0 -Force
+# Отобразить "Этот компьютер" на рабочем столе
+New-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\HideDesktopIcons\NewStartPanel -Name "{20D04FE0-3AEA-1069-A2D8-08002B30309D}" -PropertyType DWord -Value 0 -Force
+# Не показывать панель "Люди" на панели задач
+IF (-not (Test-Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People))
+{
+	New-Item -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People -Force
+}
+New-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\People -Name PeopleBand -PropertyType DWord -Value 0 -Force
+# Не показывать кнопку Windows Ink Workspace на панели задач
+New-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\PenWorkspace -Name PenWorkspaceButtonDesiredVisibility -PropertyType DWord -Value 0 -Force
+# Скрыть поле или значок поиска на Панели задач
+New-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Search -Name SearchboxTaskbarMode -PropertyType DWord -Value 0 -Force
+# Удалить OneDrive
+Stop-Process -Name OneDrive -Force -ErrorAction SilentlyContinue
+Start-Process -FilePath "$env:SystemRoot\SysWOW64\OneDriveSetup.exe" -ArgumentList "/uninstall" -Wait
+Stop-Process -Name explorer
+Remove-ItemProperty -Path HKCU:\Environment -Name OneDrive -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "$env:ProgramData\Microsoft OneDrive" -Recurse -Force -ErrorAction SilentlyContinue
+Unregister-ScheduledTask -TaskName *OneDrive* -Confirm:$false
+IF ((Get-ChildItem -Path $env:USERPROFILE\OneDrive -ErrorAction SilentlyContinue | Measure-Object).Count -eq 0)
+{
+	Remove-Item -Path $env:USERPROFILE\OneDrive -Recurse -Force -ErrorAction SilentlyContinue
+}
+else
+{
+	Write-Error "$env:USERPROFILE\OneDrive folder is not empty"
+}
+Wait-Process -Name OneDriveSetup -ErrorAction SilentlyContinue
+Remove-Item -Path $env:LOCALAPPDATA\Microsoft\OneDrive -Recurse -Force -ErrorAction SilentlyContinue
+$Error.RemoveAt(0)
+# Не показывать рекомендации в меню "Пуск"
+New-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\ContentDeliveryManager -Name SubscribedContent-338388Enabled -PropertyType DWord -Value 0 -Force
+# Скрыть папку "Объемные объекты" из "Этот компьютер" и на панели быстрого доступа
+IF (-not (Test-Path -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{31C0DD25-9439-4F12-BF41-7FF4EDA38722}\PropertyBag"))
+{
+	New-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{31C0DD25-9439-4F12-BF41-7FF4EDA38722}\PropertyBag" -Force
+}
+New-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\FolderDescriptions\{31C0DD25-9439-4F12-BF41-7FF4EDA38722}\PropertyBag" -Name ThisPCPolicy -PropertyType String -Value Hide -Force
+
+
+# Копирует AA на раб.стол
+Copy-Item C:\install\AA_v3.exe C:\Users\admin\Desktop
+
+# Установка TeamViewer
+Copy-Item -Path C:\install\TeamViewer\ -Destination C:\Users\admin\ -Recurse
+
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("C:\Users\admin\Desktop\TeamViewer.lnk")
+$Shortcut.TargetPath = "C:\Users\admin\TeamViewer\App\TeamViewer\TeamViewer.exe"
+$Shortcut.Save()
+
+cd C:\Users\admin\TeamViewer\App\TeamViewer\
+.\TeamViewer.exe
+cd C:\
+
+#Настройка учетной записи
+netplwiz
